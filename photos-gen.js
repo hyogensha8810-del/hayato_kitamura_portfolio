e4057eb20bf9792b3bb108b12b36cbff63dc96d4
hyogensha8810-del <305847432+hyogensha8810-del@users.noreply.github.com>
2026-08-06 00:39:23 +0000

    chore: auto-optimize images & update photo list [skip ci]

--- photos-gen.js
+++ photos-gen.js
@@ -7,7 +7,11 @@
    ========================================================= */
 
 const SITE_PHOTOS_AUTO = {
-  gourmet: [],
+  gourmet: [
+    { src: "photos/gourmet/01_gourmet_hyogensha_-53.jpg", alt: "HAYATO KITAMURA 作品｜グルメ写真 001" },
+    { src: "photos/gourmet/02_gourmet_hyogensha_-52.jpg", alt: "HAYATO KITAMURA 作品｜グルメ写真 002" },
+    { src: "photos/gourmet/03_gourmet_hyogensha_-2.jpg", alt: "HAYATO KITAMURA 作品｜グルメ写真 003" }
+  ],
   sweets: [
     { src: "photos/sweets/gourmet_hyogensha_-40.jpg", alt: "HAYATO KITAMURA 作品｜スイーツ写真 001" },
     { src: "photos/sweets/gourmet_hyogensha_-41.jpg", alt: "HAYATO KITAMURA 作品｜スイーツ写真 002" }
